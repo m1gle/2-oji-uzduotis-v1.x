c961d6678671c0a7e8cf1915323b01c304d95154
m1gle <78843923+m1gle@users.noreply.github.com>
2021-02-18 22:24:43 +0200

    Create header.h

---
+++ header.h
@@ -0,0 +1,24 @@
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <stdio.h>
+#include <string>
+#include <stdlib.h>
+#include <vector>
+#include <limits>
+#include <iterator>
+#include <bits/stdc++.h> 
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::setw;
+using std::setprecision;
+using std::vector;
+using std::string;
+
+
+#endif // HEADER_H_INCLUDED
