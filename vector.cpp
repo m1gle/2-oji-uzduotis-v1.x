a181eba87cd65f6f5263ad4fcb0970f6bb6420ca
m1gle <78843923+m1gle@users.noreply.github.com>
2021-03-17 20:18:50 +0200

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,43 +1,30 @@
 #include "header.h"
 
 int main(){
-vector<Studentas> sarasas;
+vector<Studentas> sarasas, vargsiukai, galvociai;
 int kiek,pazymiai,x,size;
 string ats;
 bool error,repeat;
 string input;
 
-do {
-    cout<<"Skaityti faila (y) ar ivesti ranka (n) ?: ";
-    cin>>ats;
-    error=false;
-    if(ats!="Y" && ats!="y" && ats!="n" && ats!="N"){
-        error = true;
-        cout<<"Klaida! Netinkamas ivedimas. "<<endl;
-    }
-cin.clear();
-cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-}while(error);
-
-if(ats=="y" || ats=="Y"){
+auto start = std::chrono::high_resolution_clock::now(); auto st=start;
+kurimas();
 try {
     skaitymas(sarasas,size);
     } catch (const char* msg) {
     std::cerr << msg << endl;
     exit (EXIT_FAILURE);
 }
-}
 
-if(ats=="n" || ats=="N"){  
-vedimasRanka(sarasas,size);
-}      
-
-medianaGalutinis(sarasas,size);
 vidurkisGalutinis(sarasas,size);
 rikiavimas(sarasas,size);
-spausdinimas(sarasas,size);
-
-sarasas.erase (sarasas.begin(),sarasas.begin()+size);
-
+rusiavimas(sarasas,vargsiukai,galvociai);
+vSpausdinimas(vargsiukai,true);
+gSpausdinimas(galvociai,true);
+auto end = std::chrono::high_resolution_clock::now();
+std::chrono::duration<double> diff = end-start; // Skirtumas (s)
+cout<<"Bendrai uztruko: "<<diff.count()<<endl;
+galvociai.erase (galvociai.begin(),galvociai.begin()+galvociai.size());
+vargsiukai.erase (vargsiukai.begin(),vargsiukai.begin()+vargsiukai.size());
 return 0;
 }
