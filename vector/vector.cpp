0075e5ead2fe4d3a0571dc367350d77192b584bb
m1gle <78843923+m1gle@users.noreply.github.com>
2021-04-01 19:26:54 +0300

    Create vector.cpp

---
+++ vector/vector.cpp
@@ -0,0 +1,24 @@
+#include "header.h"
+
+int main(){
+vector<Studentas> sarasas, vargsiukai, galvociai;
+int kiek,pazymiai,x,size;
+string ats;
+bool error,repeat;
+string input;
+kurimas();
+try {
+    skaitymas(sarasas,size);
+    } catch (const char* msg) {
+    std::cerr << msg << endl;
+    exit (EXIT_FAILURE);
+}
+
+vidurkisGalutinis(sarasas,size);
+rusiavimas(sarasas,vargsiukai,galvociai);
+vSpausdinimas(vargsiukai,true);
+gSpausdinimas(galvociai,true);
+galvociai.erase (galvociai.begin(),galvociai.begin()+galvociai.size());
+vargsiukai.erase (vargsiukai.begin(),vargsiukai.begin()+vargsiukai.size());
+return 0;
+}
