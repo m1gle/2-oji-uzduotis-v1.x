2022a3740ebaa96ad1aaec47dae547c256c3bbde
m1gle <78843923+m1gle@users.noreply.github.com>
2021-04-15 22:18:02 +0300

    Create vector.cpp

---
+++ 1_Strategija/vector.cpp
@@ -0,0 +1,35 @@
+#include "header.h"
+
+int main(){
+vector<Studentas> sarasas, vargsiukai, galvociai;
+int kiek,pazymiai,x,size;
+string ats;
+bool error,repeat;
+string input;
+//kurimas();
+int N = 100;
+for(int i = 0; i < 5 ; i++) {
+cout<<N<<" studentu dydzio failas: "<<endl;
+auto start = std::chrono::high_resolution_clock::now(); auto st=start;
+try {
+    skaitymas(sarasas,size,N);
+    } catch (const char* msg) {
+    std::cerr << msg << endl;
+    exit (EXIT_FAILURE);
+}
+
+vidurkisGalutinis(sarasas,size);
+rusiavimas(sarasas,vargsiukai,galvociai);
+sarasas.clear();
+vSpausdinimas(vargsiukai,true,N);
+gSpausdinimas(galvociai,true,N);
+auto end = std::chrono::high_resolution_clock::now();
+std::chrono::duration<double> diff = end-start; // Skirtumas (s)
+cout<<N<<" vector bendrai uztruko: "<<diff.count()<<endl;
+cout<<endl;
+galvociai.erase (galvociai.begin(),galvociai.begin()+galvociai.size());
+vargsiukai.erase (vargsiukai.begin(),vargsiukai.begin()+vargsiukai.size());
+N=N*10;
+}
+return 0;
+}
