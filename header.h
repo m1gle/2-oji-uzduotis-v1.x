ad90fbf0bc1bbc8064dc081465f1faa18afd2cb3
m1gle <78843923+m1gle@users.noreply.github.com>
2021-05-07 08:59:12 +0300

    Update header.h

--- header.h
+++ header.h
@@ -14,6 +14,7 @@
 #include <fstream>
 #include <algorithm>
 #include <chrono>
+#include <iostream>
 
 using std::cin;
 using std::cout;
@@ -23,50 +24,48 @@ using std::setprecision;
 using std::vector;
 using std::string;
 
-//const int N= 100000;
-const int nd= 5;
-
-// Reikia includinti visas priklausomybes: <iostream>, <string>, <vector>, "mediana.h"
 class Studentas {
-// realizacija
 private:
   string vardas;
   string pavarde;
   int egzaminas;
   vector<int> nd;
   int galutinis;
-// interfeisas
+  
 public:
+// Konstruktoriai:
+    Studentas(){
+        setReiksmes("a","b",10,10);
+    }
+// Destruktorius:
+    Studentas::~Studentas(){}
 // Seteriai:
     void setVardas(string vardas){this->vardas = vardas;}
     void setPavarde (string pavarde){this->pavarde = pavarde;}
     void setEgzaminas (int egzaminas){this->egzaminas = egzaminas;}
     void setPazymys (int pazymys){nd.push_back(pazymys);}
     void setGalutinis (double galutinis){this->galutinis = galutinis;}
+    void setReiksmes(string vardas, string pavarde, int egzaminas, int galutinis){ // vienas konstruktorius užpildyti visą vektorių:
+        this->vardas = vardas;
+        this->pavarde = pavarde;
+        this->egzaminas = egzaminas;
+        this->galutinis = galutinis;
+    }
 // Geteriai:
-    string getVardas (){
-       return vardas;
-    }
-    string getPavarde (){
-       return pavarde;
-    }
-    int getEgzaminas (){
-       return egzaminas;
-    }
-    vector<int> getPazymiai (){
-        return nd;
-    }
-    int getPazymys (){
-        return nd[0];
-    }
+    string getVardas (){return vardas;}
+    string getPavarde (){return pavarde;}
+    int getEgzaminas (){return egzaminas;}
+    vector<int> getPazymiai (){return nd;}
+    int getPazymys (){return nd[0];}
     int getGalutinis() {return galutinis;}
-
 };
 
+
 void skaitymas(vector <Studentas> &sarasas, int n);
-double vidurkisGalutinis (vector<Studentas> &sarasas);
+void vidurkisGalutinis (vector<Studentas> &sarasas);
 void rusiavimas (vector<Studentas> sarasas, vector<Studentas> &vargsiukai, vector<Studentas> &galvociai);
 void vSpausdinimas (vector<Studentas> vargsiukai, int n);
 void gSpausdinimas (vector<Studentas> galvociai, int n);
+
 
 #endif 
